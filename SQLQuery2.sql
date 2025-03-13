1394a0452ec1c8963eddc93384b4e675884e844b
minhngoc01221 <ngocnguyenminh779@gmail.com>
2025-03-13 19:11:50 +0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.Student VALUES ('A1009',N'NGUYEN MINH NGOC',2000,'Male','Ha Noi')  
+INSERT INTO dbo.Student VALUES ('A1006',N'NGUYEN MINH A',1999,'Male','Ha Noi')
+INSERT INTO dbo.Student VALUES ('A1004',N'NGUYEN VAN THANG',1970,'Male','Ha Noi')
+Select * From dbo.Student
+
+
+delete dbo.Student
